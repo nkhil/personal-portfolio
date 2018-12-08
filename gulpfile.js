910a1e85247c2a2dcab0c1f523f2c7b51211c05b
Nikhil Vijayan <nkhilv@gmail.com>
2018-12-08 00:20:55 +0000

    Added html minification && custom path re-writing

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var bs = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+var htmlreplace = require('gulp-html-replace');
+
+gulp.task('browser-sync', ['sass'], function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+                .pipe(sass().on('error', sass.logError))
+                .pipe(cleanCSS({compatibility: 'ie8'}))
+                .pipe(gulp.dest('dist/css'))
+                .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('minify', () => {
+    return gulp.src('./*.html')
+    .pipe(htmlreplace({
+        'css': './css/style.css'
+    }))
+    .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
+});
